1861c926d0045aaafe82e3543981b6c0c9ea3f46
Tanuj Ravi Rao <tanujrao99@gmail.com>
2024-07-12 14:36:19 +0000

    not dealing with seafile anymore

--- kubernetes/apps/database/seafile-mariadb/app/check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ec
-
-password_aux="${MARIADB_ROOT_PASSWORD:-}"
-if [[ -f "${MARIADB_ROOT_PASSWORD_FILE:-}" ]]; then
-    password_aux=$(cat "$MARIADB_ROOT_PASSWORD_FILE")
-fi
-mysqladmin status -uroot -p"${password_aux}"
